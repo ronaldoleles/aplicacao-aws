abcf4b2da53b3f12dc880943c9fb29812c88a910
Ronaldo Júnio Barbosa de Leles <ronalldoa@hotmail.com>
2022-01-26 17:05:22 -0300

    teste de conexão com servidor

---
+++ index.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+const express = require ('express');
+
+const app = express();
+
+app.get('/',(request, response)=>{
+  return response.json({message:'Server is up'})
+})
+
+app.listen(80)

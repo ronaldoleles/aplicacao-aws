497a7491a284a959f2b790cc360dec2cd28eeaab
Ronaldo Júnio Barbosa de Leles <ronalldoa@hotmail.com>
2022-01-26 17:15:45 -0300

    Att index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require ('express');
 
 const app = express();
 
 app.get('/',(request, response)=>{
-  return response.json({message:'Server is up'})
+  return response.json({message:'Server is up'});
+})
+
+app.post('/teste',(request,response)=>{
+  const{name,date} = request.body;
+  return response.json({name,date});
 })
 
 app.listen(80)
